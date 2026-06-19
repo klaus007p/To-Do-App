6041540cb93383ce9ba0fcad18f33113896d4acc
Prabhat Pathak <klaus007p@gmail.com>
2026-06-19 18:47:12 +0530

    Finished the todo app project with drag and drop features the tasks that are added will be stored directly on the local storage

--- script.js
+++ script.js
@@ -6,17 +6,80 @@ const progress = document.querySelector('#progress');
 const done = document.querySelector('#done');
 let dragElement = null;
 
+function addTask(title, taskDescription, column) {
+    const div = document.createElement('div')
+
+    div.classList.add('task')
+    div.setAttribute('draggable', 'true')
+
+    div.innerHTML = `
+    <h2>${title}</h2>
+    <p>${taskDescription}</p>
+    <button>Delete</button>`
+
+    column.appendChild(div)
+
+    div.addEventListener("drag", (e) => {
+        dragElement = div;
+    })
+
+    const deleteBtn = div.querySelector('button');
+    deleteBtn.addEventListener('click',() =>{
+        div.remove();
+        updateTaskCount();
+    })
+
+    return div;
+}
+
+
+function updateTaskCount() {
+
+    [todo, progress, done].forEach(col => {
+        const task = col.querySelectorAll(".task");
+        const count = col.querySelector('.right');
+
+        taskData[col.id] = Array.from(task).map(t => {
+            return {
+                title: t.querySelector('h2').innerText,
+                desc: t.querySelector('p').innerText
+            }
+        })
+
+        localStorage.setItem('task', JSON.stringify(taskData))
+
+        count.innerText = task.length;
+    })
+}
+
+if (localStorage.getItem('task')) {
+    const data = JSON.parse(localStorage.getItem('task'));
+
+    for (const col in data) {
+        const column = document.querySelector(`#${col}`);
+        data[col].forEach(task => {
+            addTask(task.title, task.desc, column);
+
+        })
+
+    }
+
+    updateTaskCount()
+}
+
+
+
 const tasks = document.querySelectorAll('.task');
 
-tasks.forEach(task =>{
-    task.addEventListener("drag", (e) =>{
-       dragElement = task
+tasks.forEach(task => {
+    task.addEventListener("drag", (e) => {
+        dragElement = task
     })
 })
 
 // progress.addEventListener("dragenter", (e) =>{
 //     progress.classList.add("hover-over")
-    
+
 // })
 
 // progress.addEventListener("dragleave", (e) =>{
@@ -25,36 +88,32 @@ tasks.forEach(task =>{
 
 
 
-function addDragEventsOnColumn(column){
-    column.addEventListener("dragenter",(e) =>{
+function addDragEventsOnColumn(column) {
+    column.addEventListener("dragenter", (e) => {
         e.preventDefault();
         column.classList.add("hover-over");
     })
-    column.addEventListener("dragleave",(e) =>{
+    column.addEventListener("dragleave", (e) => {
         e.preventDefault();
         column.classList.remove("hover-over");
     })
 
-    column.addEventListener("dragover",(e) =>{
+    column.addEventListener("dragover", (e) => {
         e.preventDefault();
     })
 
-    column.addEventListener("drop", (e) =>{
+    column.addEventListener("drop", (e) => {
         e.preventDefault();
-        
-        console.log("drop", dragElement,column);
-        
+
+
+
         column.appendChild(dragElement);
         column.classList.remove("hover-over");
 
-        [todo,progress,done].forEach(col =>{
-            const task = col.querySelectorAll(".task");
-            const count = col.querySelector('.right');
 
-            count.innerText = task.length;
-        })
 
-        
+        updateTaskCount()
+
     })
 }
 
@@ -70,52 +129,24 @@ const addTaskBtn = document.querySelector("#add-new-task");
 
 
 
-toggleModalBtn.addEventListener("click", () =>{
+toggleModalBtn.addEventListener("click", () => {
     modal.classList.toggle("active");
 });
 
-modalBg.addEventListener("click", () =>{
+modalBg.addEventListener("click", () => {
     modal.classList.remove("active");
 })
 
-addTaskBtn.addEventListener("click", () =>{
-    
+addTaskBtn.addEventListener("click", () => {
+
     const taskTitle = document.querySelector("#task-title-input").value
     const taskDescription = document.querySelector("#task-desc-input").value
 
-    const div = document.createElement('div')
-    div.classList.add("task")
-    div.setAttribute("draggable", "true")
 
-    div.innerHTML = `
-        <h2>${taskTitle}</h2>
-        <p>${taskDescription}</p>
-        <button>Delete</button>`
+    addTask(taskTitle, taskDescription, todo);
+    updateTaskCount();
+    modal.classList.remove("active")
 
-        todo.appendChild(div);
-
-        
-
-
-        [todo,progress,done].forEach(col =>{
-            const task = col.querySelectorAll(".task");
-            const count = col.querySelector('.right');
-
-            taskData[col.id] = Array.from(task).map(t =>{ 
-                return{
-                    title: t.querySelector('h2').innerText,
-                    desc: t.querySelector('p').innerText
-                }
-            })
-
-            localStorage.setItem('task', JSON.stringify(taskData))
-
-            count.innerText = task.length;
-        })
-
-        div.addEventListener("drag", (e)=>{
-            dragElement = div;
-        })
-
-        modal.classList.remove("active")
+    document.querySelector('#task-title-input').value = "";
+    document.querySelector('#task-desc-input').value = "";
 })
